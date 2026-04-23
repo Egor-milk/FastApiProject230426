c788c96eadcacea90043c9d17893b4781864f313
Egor-milk <mrpixelegor@gmail.com>
2026-04-23 18:10:33 +0700

    добавил delete endpoint

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi import Query
+
 app = FastAPI()
 
+hotels = [
+    {'id': 1, 'title': 'Sochi'},
+    {'id': 2, 'title': 'Dubai'},
+]
 
-@app.get("/")
-def func():
-    return "Hello World!!!"
+
+@app.get("/hotels")
+def get_hotels(
+        id: int | None = Query(None, description='Айдишник'),
+        title: str | None = Query(None, description='Название отеля'),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel['id'] != id:
+            continue
+        if title and hotel['title'] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
+    return {'status': 'ok'}
+
 
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
