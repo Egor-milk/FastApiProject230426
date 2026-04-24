5d939833b3cdff932b6348d993e1c25449756ea7
Egor-milk <mrpixelegor@gmail.com>
2026-04-24 17:11:19 +0700

    рефакторинг кода, оказывается не было поля "name" в hotels

--- main.py
+++ main.py
@@ -1,75 +1,79 @@
+from fastapi import FastAPI, Query, Body
 import uvicorn
-from fastapi import FastAPI
-from fastapi import Query, Body
 
 app = FastAPI()
 
 hotels = [
-    {'id': 1, 'title': 'Sochi'},
-    {'id': 2, 'title': 'Dubai'},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
 ]
 
 
 @app.get("/hotels")
 def get_hotels(
-        id: int | None = Query(None, description='Айдишник'),
-        title: str | None = Query(None, description='Название отеля'),
+        id: int | None = Query(None, description="Айдишник"),
+        title: str | None = Query(None, description="Название отеля"),
 ):
     hotels_ = []
     for hotel in hotels:
-        if id and hotel['id'] != id:
+        if id and hotel["id"] != id:
             continue
-        if title and hotel['title'] != title:
+        if title and hotel["title"] != title:
             continue
         hotels_.append(hotel)
     return hotels_
 
+
 @app.post("/hotels")
 def create_hotel(
-        title: str = Body(embed=True), #embed = бэк будет ожидать json а не просто строку
+        title: str = Body(embed=True),
 ):
     global hotels
-    hotels.append({'id': hotels[-1]['id'] + 1, 'title': title})
+    hotels.append({
+        "id": hotels[-1]["id"] + 1,
+        "title": title
+    })
+    return {"status": "OK"}
+
+
+@app.put("/hotels/{hotel_id}")
+def edit_hotel(
+        hotel_id: int,
+        title: str = Body(),
+        name: str = Body(),
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    hotel["title"] = title
+    hotel["name"] = name
+    return {"status": "OK"}
+
+
+@app.patch(
+    "/hotels/{hotel_id}",
+    summary="Частичное обновление данных об отеле",
+    description="<h1>Тут мы частично обновляем данные об отеле: можно отправить name, а можно title</h1>",
+)
+def partially_edit_hotel(
+        hotel_id: int,
+        title: str | None = Body(None),
+        name: str | None = Body(None),
+):
+    global hotels
+    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
+    if title:
+        hotel["title"] = title
+    if name:
+        hotel["name"] = name
+    return {"status": "OK"}
+
 
 @app.delete("/hotels/{hotel_id}")
 def delete_hotel(hotel_id: int):
     global hotels
-    hotels = [hotel for hotel in hotels if hotel['id'] != hotel_id]
-    return {'status': 'ok'}
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status": "OK"}
 
-@app.put('/hotels/{hotel_id}')
-def put_hotel(
-    hotel_id: int,
-    new_hotel_id : int = Body(embed=True),
-    new_title: str = Body(embed=True),
-):
-    global hotels
-    hotels_ = []
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            hotel['id'] = new_hotel_id
-            hotel['title'] = new_title
-        hotels_.append(hotel)
-    hotels = hotels_
-    return {'status': 'ok'}
-
-@app.patch('/hotels/{hotel_id}')
-def patch_hotel(
-    hotel_id: int,
-    new_hotel_id : int | None = Body(embed=True),
-    new_title: str | None = Body(embed=True),
-):
-    global hotels
-    hotels_ = []
-    for hotel in hotels:
-        if hotel['id'] == hotel_id:
-            if new_hotel_id:
-                hotel['id'] = new_hotel_id
-            if new_title:
-                hotel['title'] = new_title
-        hotels_.append(hotel)
-    hotels = hotels_
-    return {'status': 'ok'}
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run("main:app", reload=True)
