99e3ef5d46692949e3d743e8e99b3af41c0b7ebf
Egor-milk <mrpixelegor@gmail.com>
2026-04-25 18:43:42 +0700

    начал работу с postgresql

--- src/config.py
+++ src/config.py
@@ -2,7 +2,18 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HOST: str
     DB_NAME: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+
+    #DSN
+    @property
+    def DB_URL(self):
+        f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
     model_config = SettingsConfigDict(env_file='.env')
 
--- src/main.py
+++ src/main.py
@@ -9,7 +9,12 @@ sys.path.append(str(Path(__file__).parent.parent)) #костыль, скорее
 from src.api.hotels import router as router_hotels
 from src.config import settings
 
-print(f'{settings.DB_NAME=}')
+
+print(f'{settings.DB_URL=}')
+
+
+
+
 app = FastAPI()
 
 app.include_router(router_hotels, tags=["hotels"])
