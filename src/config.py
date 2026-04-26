d7a094aec4f49cc5916fef4db3cdd12c50d50156
Egor-milk <mrpixelegor@gmail.com>
2026-04-26 15:41:52 +0700

    написал тестовый сырой sql запрос для проверки работоспособности

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     #DSN
     @property
     def DB_URL(self):
-        f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
     model_config = SettingsConfigDict(env_file='.env')
 
--- src/database.py
+++ src/database.py
@@ -1,4 +1,17 @@
+import asyncio
+
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy import text
+
+from src.config import settings
 
 
-engine = create_async_engine('', future=True)
+engine = create_async_engine(settings.DB_URL)
+
+#test
+async def func():
+    async with engine.begin() as conn:#начало транзакции
+        res = await conn.execute(text('SELECT version()'))
+        print(res.fetchone())
+
+asyncio.run(func())
