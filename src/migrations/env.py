911b58dc20e6999f24aba10aa56085596652dc9d
Egor-milk <mrpixelegor@gmail.com>
2026-05-04 18:28:16 +0700

    создал модель bookings, выполнил миграцию

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -12,7 +12,8 @@ from src.config import settings
 from src.database import Base
 from src.models.hotels import HotelsOrm #нужно обязательно импортировать с Base
 from src.models.rooms import RoomsOrm
-from src.models.users import UsersOrm 
+from src.models.users import UsersOrm
+from src.models.bookings import BookingsOrm 
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
