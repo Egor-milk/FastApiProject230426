999a721ca540b3c69e58bced0e5b8a931f106aea
Egor-milk <mrpixelegor@gmail.com>
2026-05-12 15:04:18 +0700

    реализовал post /images, ендпоинт принимает фото и конвертирует его в несколько других форматов, задача работает через celery

--- src/main.py
+++ src/main.py
@@ -18,6 +18,7 @@ from src.api.auth import router as router_auth
 from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
+from src.api.images import router as router_images
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -35,5 +36,7 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_bookings)
 app.include_router(router_facilities)
+app.include_router(router_images)
+
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
