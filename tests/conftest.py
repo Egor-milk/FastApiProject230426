6064a4b5868b7fa0f0be595d9203a5acaa2f6d04
Egor-milk <mrpixelegor@gmail.com>
2026-05-13 13:54:17 +0700

    создал фикстуру для регистрации user`а, использовал библиотеку httpx

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import pytest
 
 from src.config import settings
-from src.database import Base, engine, engine_null_pull
+from src.database import Base, engine_null_pull
+from src.main import app as app1
 from src.models import * #__init__.py
+
+from httpx import AsyncClient, ASGITransport
+
 
 @pytest.fixture(scope="session", autouse=True)
 def check_test_mode():
@@ -10,9 +14,24 @@ def check_test_mode():
 
 #session - сессия тестирования
 @pytest.fixture(scope="session", autouse=True) #autouse - автоматический запуск кода при вызове pytest в консоли
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    transport = ASGITransport(app=app1)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "test@mail.ru",
+                "password": "1234",
+            }
+        )
+
+
+
 
 # pytest -v -s
