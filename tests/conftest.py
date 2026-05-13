53a127f1bb8a493459568e265325f829e6c4a6b9
Egor-milk <mrpixelegor@gmail.com>
2026-05-13 13:07:36 +0700

    разбил фикстуру на две, вынес проверку подключения к бд в тестовом моде в отдельный тест

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,11 +4,15 @@ from src.config import settings
 from src.database import Base, engine, engine_null_pull
 from src.models import * #__init__.py
 
+@pytest.fixture(scope="session", autouse=True)
+def check_test_mode():
+    assert settings.MODE == "TEST"
+
 #session - сессия тестирования
 @pytest.fixture(scope="session", autouse=True) #autouse - автоматический запуск кода при вызове pytest в консоли
-async def async_main():
-    print('я фикстура')
-    assert settings.MODE == "TEST"
+async def async_main(check_test_mode):
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+# pytest -v -s
