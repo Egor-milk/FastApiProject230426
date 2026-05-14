7ec7952feccaf48f90bbe93d37a22b0f32e59d5a
Egor-milk <mrpixelegor@gmail.com>
2026-05-14 18:08:47 +0700

    параметризировал api тесты bookings

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,17 +1,32 @@
+import pytest
 
-
-async def test_add_booking(db, authenticated_ac):
-    room_id = (await db.rooms.get_all())[0].id
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2026-01-01", "2026-02-01", 200),
+    (1, "2026-01-01", "2026-02-01", 200),
+    (1, "2026-01-01", "2026-02-01", 200),
+    (1, "2026-01-01", "2026-02-01", 200),
+    (1, "2026-01-01", "2026-02-01", 200),
+    (1, "2026-01-01", "2026-02-01", 409),
+    (1, "2026-01-01", "2026-02-01", 409),
+    (1, "2026-01-01", "2026-02-01", 409),
+    (1, "2026-01-01", "2026-02-01", 409),
+    (1, "2026-01-01", "2026-02-01", 409),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, authenticated_ac):
+    #room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
         f"/bookings",
         json={
             "room_id": room_id,
-            "date_from": "2026-01-01",
-            "date_to": "2026-02-01",
+            "date_from": date_from,
+            "date_to": date_to,
         }
     )
-    assert response.status_code == 200
-    res = response.json()
-    assert isinstance(res, dict)
-    assert res["status"] == "ok"
-    assert "data" in res
+    assert response.status_code == status_code
+    if status_code == 200:
+        res = response.json()
+        assert isinstance(res, dict)
+        assert res["status"] == "ok"
+        assert "data" in res
